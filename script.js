5262f9c8fc0fe42607b2f9a6d62646d974170d4e
mcmorriss <michaelmorriss14@gmail.com>
2023-04-28 01:31:58 -0500

    typing animation using typed.js added

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ MenuBtn.addEventListener('click', function(e) {
     document.querySelector('body').classList.toggle('mobile-nav-active');
     this.classList.toggle('fa-xmark')
 })
+
+// Typing effect
+let typed = new Typed('.auto-input', {
+    strings: ['Software Engineer .', 'Full Stack Developer .', 'Freelance Developer .', ''],
+    typeSpeed: 75,
+    backSpeed: 50,
+    backDelay: 2000,
+    loop: true
+})
